c104c7dad804bcbbfeeba14996ebb0e9c000dc38
yhhe <yanhe0101@163.com>
2026-06-03 16:51:43 +0800

    refactor(auth): remove legacy LoginController, JwtTokenService, JwksController, RemoteUserService

--- daydayup-platform/daydayup-auth/src/main/java/com/yuan/daydayup/auth/mapper/RefreshTokenMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yuan.daydayup.auth.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yuan.daydayup.auth.entity.RefreshToken;
-
-public interface RefreshTokenMapper extends BaseMapper<RefreshToken> {
-}
