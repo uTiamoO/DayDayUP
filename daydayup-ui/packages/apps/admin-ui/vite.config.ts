c105ab26c124f90d6cf3fda13d9ba6a9c8275547
yhhe <yanhe0101@163.com>
2026-06-18 18:28:47 +0800

    fix(admin-ui): 使用 ESM 标准配置 vite.config.ts 并补充 @types/node 依赖

--- daydayup-ui/packages/apps/admin-ui/vite.config.ts
+++ daydayup-ui/packages/apps/admin-ui/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
 // 配置 Vite 并引入 Vue 插件
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 });
